a47ec60d305544758056662ec8aa15357bef5bd7
misspran <vipran@gmail.com>
2014-10-24 11:41:12 -0500

    first commit

---
+++ Person.h
@@ -0,0 +1,27 @@
+//
+//  Person.h
+//  Assessment4
+//
+//  Created by Vi on 10/24/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Dog;
+
+@interface Person : NSManagedObject
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSSet *dogs;
+@end
+
+@interface Person (CoreDataGeneratedAccessors)
+
+- (void)addDogsObject:(Dog *)value;
+- (void)removeDogsObject:(Dog *)value;
+- (void)addDogs:(NSSet *)values;
+- (void)removeDogs:(NSSet *)values;
+
+@end
---
+++ Person.m
@@ -0,0 +1,18 @@
+//
+//  Person.m
+//  Assessment4
+//
+//  Created by Vi on 10/24/14.
+//  Copyright (c) 2014 MobileMakers. All rights reserved.
+//
+
+#import "Person.h"
+#import "Dog.h"
+
+
+@implementation Person
+
+@dynamic name;
+@dynamic dogs;
+
+@end
